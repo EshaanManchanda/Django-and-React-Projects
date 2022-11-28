e7dc934e82e552b6b18e3999f8603c6d411f87da
Eshaan <eshaanmanchanda01@gmail.com>
2022-11-28 13:00:26 +0530

    successfully link react and django

--- DjangoPro/DjangoPro/settings.py
+++ DjangoPro/DjangoPro/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-km1+#qbl%qxv=ep*z67w9c6p11nsii+(_5x5fz(d-d*cz1%rta'
+SECRET_KEY = 'django-insecure-y6kl#q8%8hk_u961-dbcxzw%b!=#jp)-t3cdv4&7r-uh2#h%p8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'DjangoPro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'reactapp/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS =[os.path.join(BASE_DIR, 'reactapp/build/static')]
